fe3019667117ad20bd520e9e33787732ad42bfc2
Alreyesh <alfreyh@gmail.com>
2023-04-21 19:38:35 -0500

    21/04/2023

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { AndroidComponent } from './pages/android/android.component';
+import { WebComponent } from './pages/web/web.component';
+const routes: Routes = [
+{ path: 'home', component: HomeComponent},
+{path:'android',component: AndroidComponent},
+{path:'web',component: WebComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AndroidComponent } from './pages/android/android.component';
+import { WebComponent } from './pages/web/web.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AndroidComponent,
+    WebComponent
   ],
   imports: [
     BrowserModule,
